481b5a1877f7321a1de897dd0efd01ea8956e150
Chandrarit <130826166+crm16@users.noreply.github.com>
2025-06-29 20:19:28 +0530

    Add files via upload

---
+++ ascending_descending_check.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class ascending_descending_check{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements to be checked: ");
+        int size = sc.nextInt();
+
+        int numbers[] = new int[size];
+        System.out.println("Enter " + size + " elements: ");
+        for(int i =0 ; i<size; i++){
+            numbers[i] = sc.nextInt();
+        }
+        boolean isDescending = true;
+        boolean isAscending = true;
+        for(int i = 0 ; i<numbers.length-1; i++){
+            if(numbers[i]<numbers[i+1]){
+                isDescending = false;
+            }
+            if(numbers[i]>numbers[i+1]){
+                isAscending = false;
+            }
+        }
+        if(isAscending){
+            System.out.println("Elements are sorted in ascending order...");
+        }
+        else if(isDescending){
+            System.out.println("Elements are sorted in descending order...");
+        }
+        else{
+            System.out.println("Elements are not sorted....");
+        }
+    }
+}
